bc37d8faeeb4d9ef1ee95b7f13b1819c2ad822f3
Vitaly <79vitaly79@gmail.com>
2018-05-03 22:36:32 +0300

    Added question about quotient
    Added question about quotient

--- gubar_dz.cpp
+++ gubar_dz.cpp
@@ -4,8 +4,8 @@
 #ifdef _WIN32
 #include <tchar.h>
 #else
-  typedef char _TCHAR;
-  #define _tmain main
+typedef char _TCHAR;
+#define _tmain main
 #endif
 
 #include <stdio.h>
@@ -13,30 +13,38 @@
 #include <iostream>
 #include <math.h>
 
- int _tmain(int argc, _TCHAR* argv[]) {
-	int i = 0;
-	char answer;
-	int number[7] = {0,0,0,0,0,0,0};
-	int result = 0;
 
 
-	for (i = 0; i < 7; i++){
-		std::cout << "есть ли остаток от деления на 2?(y/n) \n";
-		std::cin >> answer;
-		std::cout << std::endl;
-
-		if (answer == 'y')
-			number[6-i] = 1;
-		else
-			number[6-i] = 0;
-	}
-
-	for (i = 0; i < 7; i++){
-		result += number[i] * pow(2.0,i);
-	}
-
-	std::cout << "see the result - " << result << "\n";
-
-	getch();
-	return 0;
+int _tmain(int argc, _TCHAR* argv[]) {
+    std::cout << "Данная программа предназначена для угадывания числа от 0 до 123. \n Задаются вопросы: остается ли остаток после деления задуманного числа на 2? Остается ли остаток после деления полученного частного на 2? И так далее. Для ответа на эти вопросы пользователю необходимо ввести y(yes/да) или n(no/нет) Для решения поставленной задачи использовать алгоритм перевода десятичных чисел в двоичные путем деления на 2\n";
+    int i = 0;
+    char answer1,answer2;
+    int number[7] = {0,0,0,0,0,0,0};
+    int result = 0;
+    while (answer2 != 'y') {
+        std::cout << "есть ли остаток от деления на 2?(y/n) \n";
+        std::cin >> answer1;
+        std::cout << std::endl;
+        if (answer1 == 'y')
+            number[i] = 1;
+        else
+            number[i] = 0;
+        i++;
+        
+        std::cout << "Равно ли частное 1?(y/n) \n";
+        std::cin >> answer2;
+        std::cout << std::endl;
+    }
+    number[i] = 1;
+    std::reverse(std::begin(number), std::end(number));
+    
+    
+    for (i = 0; i < 7; i++){
+        result += number[i] * pow(2.0,i);
+    }
+    
+    std::cout << "see the result - " << result << "\n";
+    
+    getch();
+    return 0;
 }
